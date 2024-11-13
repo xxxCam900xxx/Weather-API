dfe10e9827002d838408a3e405a6dadd918e68b9
Cameron Meile <cameronmeile@edu.tbz.ch>
2024-11-13 10:02:17 +0100

    🚧 chore: Working API Request Zürich Data

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 // The API To Fetch from Zürich Data
-// https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Z%C3%BCrich?unitGroup=metric&key=YOUR_API_KEY&contentType=json
+let apiKey = "8b3c2188b1fe57f318bdcd6231e30d94";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=en";
+// let city = "Zürich";
+// fetch(`${apiUrl}&q=${city}&appid=${apiKey}`)
+
+async function requestZuerichData() {
+    try {
+        const response = await fetch(`${apiUrl}&q=Zürich&appid=${apiKey}`)
+
+        if (!response.ok) {
+            throw new Error(`Repsonse Status: ${response.Status}`);
+        }
+
+        const jsonData = await response.json();
+        console.log(jsonData);
+    }
+    catch (error) {
+        console.error(error.message);
+    }
+}
